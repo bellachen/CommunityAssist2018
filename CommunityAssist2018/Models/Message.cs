b1c8ee5ec2cc4ffc914beda607fe69d71f904122
bellachen <bellachen.nyc@gmail.com>
2018-01-31 11:45:13 -0800

    added registration

---
+++ CommunityAssist2018/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssist2018.Models
+{
+    public class Message
+    {
+        public string MessageText { get; set; }
+        
+    }
+}
